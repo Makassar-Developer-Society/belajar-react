dce4fada6cab6a6a83630e5d5313c83297a088d1
alsanusi <malkautsars@gmail.com>
2020-08-17 13:17:43 +0800

    feat: new card and dashboard layout

---
+++ src/components/Card.jsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import AddIcon from '@material-ui/icons/Add';
+
+const useStyles = makeStyles((theme) => ({
+    cardGrid: {
+        paddingTop: theme.spacing(8),
+        paddingBottom: theme.spacing(8),
+    },
+    card: {
+        height: '100%',
+        display: 'flex',
+        maxWidth: '380px',
+        flexDirection: 'column',
+        boxShadow: '0 5px 10px rgba(154,160,185,.05), 0 15px 40px rgba(166,173,201,.2)',
+        borderRadius: 10
+    },
+    cardContent: {
+        paddingLeft: theme.spacing(8),
+        flexGrow: 1,
+    },
+    textTitle: {
+        marginTop: 20,
+        fontWeight: 'bold'
+    },
+    textAmount: {
+        marginTop: 20,
+        fontWeight: 'bolder'
+    },
+    textDescription: {
+        fontWeight: '100'
+    }
+}));
+
+const cards = [1, 2, 3];
+
+export default function Dashboard() {
+    const classes = useStyles();
+
+    return (
+        <div>
+            <Container className={classes.cardGrid}>
+                <Grid container spacing={4}>
+                    {cards.map((card) => (
+                        <Grid item key={card} xs={12} md={6}>
+                            <Card className={classes.card}>
+                                <CardContent className={classes.cardContent}>
+                                    <Typography variant="h6" align="left" className={classes.textTitle} gutterBottom>
+                                        <div>
+                                            <AddIcon style={{ fontSize: 30 }} />
+                                        </div>
+                                        Case Confirmed
+                                    </Typography>
+                                    <Typography variant="h3" align="left" className={classes.textAmount}>
+                                        139,560
+                                    </Typography>
+                                    <Typography variant="subtitle2" align="left" className={classes.textDescription}>
+                                        Total of confirmed cases.
+                                    </Typography>
+                                </CardContent>
+                            </Card>
+                        </Grid>
+                    ))}
+                </Grid>
+            </Container>
+        </div>
+    );
+}
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import CovidCard from '../components/Card';
 import Footer from '../components/Footer';
 
 const background = require("../assets/img/covid-bg.png");
@@ -41,28 +37,16 @@ const useStyles = makeStyles((theme) => ({
         marginLeft: 'auto',
         marginRight: 'auto'
     },
-    cardGrid: {
-        paddingTop: theme.spacing(8),
-        paddingBottom: theme.spacing(8),
-    },
-    card: {
-        height: '100%',
-        display: 'flex',
-        flexDirection: 'column',
-    },
-    cardMedia: {
-        paddingTop: '56.25%', // 16:9
-    },
-    cardContent: {
-        flexGrow: 1,
+    textWrapper: {
+        paddingTop: theme.spacing(5),
+        marginTop: 20,
+        fontWeight: 'bolder'
     },
     footer: {
         backgroundColor: theme.palette.background.paper,
         padding: theme.spacing(6),
     },
 }));
-
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 export default function Dashboard() {
     const classes = useStyles();
@@ -92,37 +76,25 @@ export default function Dashboard() {
                     </Container>
                 </div>
                 {/* End of Header */}
-                <Container className={classes.cardGrid} maxWidth="md">
-                    <Grid container spacing={4}>
-                        {cards.map((card) => (
-                            <Grid item key={card} xs={12} sm={6} md={4}>
-                                <Card className={classes.card}>
-                                    <CardMedia
-                                        className={classes.cardMedia}
-                                        image="https://source.unsplash.com/random"
-                                        title="Image title"
-                                    />
-                                    <CardContent className={classes.cardContent}>
-                                        <Typography gutterBottom variant="h5" component="h2">
-                                            Heading
-                                        </Typography>
-                                        <Typography>
-                                            This is a media card. You can use this section to describe the content.
-                                        </Typography>
-                                    </CardContent>
-                                    <CardActions>
-                                        <Button size="small" color="primary">
-                                            View
-                                        </Button>
-                                        <Button size="small" color="primary">
-                                            Edit
-                                        </Button>
-                                    </CardActions>
-                                </Card>
-                            </Grid>
-                        ))}
+                {/* Body */}
+                <Container>
+                    <Grid container>
+                        <Grid item xs={12} md={4}>
+                            <Typography variant="h3" align="left" className={classes.textWrapper}>
+                                Indonesia
+                            </Typography>
+                            <Typography variant="subtitle2" align="left" style={{ fontWeight: '100' }}>
+                                Latest update on confirmed cases,
+                                people recovered & total death in
+                                Indonesia.
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={12} md={8}>
+                            <CovidCard />
+                        </Grid>
                     </Grid>
                 </Container>
+                {/* End Body */}
             </main>
             {/* Footer */}
             <Footer />
