b9766a8575ff17690fb37fc89b3fb2c38d5bedf0
alsanusi <malkautsars@gmail.com>
2020-08-27 20:49:13 +0700

    fix: connect card data to api

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Grid from '@material-ui/core/Grid';
@@ -8,6 +8,7 @@ import Container from '@material-ui/core/Container';
 import AddIcon from '@material-ui/icons/Add';
 import CheckIcon from '@material-ui/icons/Check';
 import RemoveIcon from '@material-ui/icons/Remove';
+import axios from 'axios';
 
 const useStyles = makeStyles((theme) => ({
     cardGrid: {
@@ -67,27 +68,48 @@ function BaseCard({ ...props }) {
 
 export default function FixedCard() {
     const classes = useStyles();
+    // Data yang akan digunakan - Set awal dengan array kosong
+    const [data, setData] = useState([])
+
+    // useEffect - merupakan lifecyle react hook yang jalan pertama kali disaat page dikunjungi / direfresh
+    useEffect(() => {
+        // Variable baru
+        let fetchData;
+        axios.get('http://api.coronatracker.com/v3/stats/worldometer/country?countryCode=ID')
+            .then(res => {
+                // ternary (if else) - Jika berhasil mendapatkan data dari api maka data diset dengan fetchData yang merupakan data dari api(res.data[0]).
+                fetchData = res ? res.data[0] : []
+                setData(fetchData)
+            })
+            .catch(err => {
+                // Jika gagal mendapatkan data dari api maka data akan diset menjadi array kosong
+                setData([])
+            })
+    }, [])
 
     const cardData = [
         {
             id: 1,
             icon: <AddIcon />,
             title: "Case Confirmed",
-            amount: "139,560",
+            // data totalConfirmed
+            amount: data.totalConfirmed ? data.totalConfirmed.toLocaleString() : 0,
             description: "Total of confirmed cases."
         },
         {
             id: 2,
             icon: <CheckIcon style={{ color: 'green' }} />,
             title: "Case Recovered",
-            amount: "93,108",
+            // data totalRecovered
+            amount: data.totalRecovered ? data.totalRecovered.toLocaleString() : 0,
             description: "Total of recovered cases."
         },
         {
             id: 3,
             icon: <RemoveIcon style={{ color: 'black' }} />,
             title: "Case Death",
-            amount: "6,150",
+            // data totalDeaths
+            amount: data.totalDeaths ? data.totalDeaths.toLocaleString() : 0,
             description: "Total of death cases."
         }
     ]
