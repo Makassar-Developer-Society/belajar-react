cd97776e55876452544ea89f7093fb3de219b72f
alsanusi <malkautsars@gmail.com>
2020-08-27 21:13:00 +0700

    Updates

---
+++ precache-manifest.83e2c521eb3174ad027940d11287cc7b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7d4e616e8b9fc9754616b2e317c2f88",
+    "url": "/belajar-react/index.html"
+  },
+  {
+    "revision": "fbf9ae3cfeda92c1e12a",
+    "url": "/belajar-react/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "1c9ec12eef677ac5d6ad",
+    "url": "/belajar-react/static/js/2.e3876ecf.chunk.js"
+  },
+  {
+    "revision": "b815bed3bedb5a8af7470bb094451139",
+    "url": "/belajar-react/static/js/2.e3876ecf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbf9ae3cfeda92c1e12a",
+    "url": "/belajar-react/static/js/main.4fd2ad61.chunk.js"
+  },
+  {
+    "revision": "31872f60cbf780c213ad",
+    "url": "/belajar-react/static/js/runtime-main.29b97b77.js"
+  },
+  {
+    "revision": "6c42c0a3c47794d5e2f2135c1e065b92",
+    "url": "/belajar-react/static/media/covid-bg.6c42c0a3.png"
+  },
+  {
+    "revision": "bba198e2ca67ac866a02cd924d85b4b6",
+    "url": "/belajar-react/static/media/person.bba198e2.png"
+  }
+]);
